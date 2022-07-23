f6ffdc5352d64b98fc2398d16310c83483d4f8e2
Tony <>
2022-07-23 13:54:36 +1000

    microservices blog mini project React

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvents = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
 
@@ -39,12 +34,35 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+}
 
-  console.log(posts);
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvents(type,data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  try {
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+      console.log('processing event: ', event.type);
+
+      handleEvents(event.type, event.data);
+    }
+
+  } catch (error) {
+    console.log(error.message);
+  }
+  
+
 });
